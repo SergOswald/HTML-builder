957923945b24e9d2b32f26dc0ce32226abb275c4
SergOswald <skobrusev446@gmail.com>
2022-05-22 11:24:10 +0300

    feat: add 01-read-file 22-05-22 11:24:30

---
+++ 01-read-file/index.js
@@ -0,0 +1,19 @@
+var fs = require('fs');
+
+const path = require('path');
+
+const path1 = path.format({
+    dir: 'C:\\IT\\RSS\\Stage1\\Serg\\HTML-builder\\01-read-file',
+    base: 'text.txt',
+});
+ 
+var stream = new fs.ReadStream( path1 );
+ 
+stream.on('readable', function(){
+    var data = stream.read();
+    console.log(data);
+});
+
+stream.on('end', function(){
+    console.log("THE END");
+});
