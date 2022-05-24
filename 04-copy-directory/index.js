1d2c6e6fa54846ab22de6ec033bfe6afca3d7fb8
SergOswald <skobrusev446@gmail.com>
2022-05-24 22:36:45 +0300

    feat: 04 last version 24-05-22 22:36

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,36 @@
+var fs = require('fs');
 
+const path = require('path');
+
+const path1 = path.format({
+    dir: './04-copy-directory/files',
+    //base: 'text.txt',
+});
+
+fs.mkdir(path.join(__dirname, 'files-copy'), (err) => {
+    if (err) {
+        return console.error(err);
+    }
+    console.log('Directory created successfully!');
+});
+
+let folderName = "./04-copy-directory/files" ;
+
+let folderNameNew = "./04-copy-directory/files-copy" ;
+
+fs.readdir(folderName, {withFileTypes: true}, (err, files) => {
+    if (err) {
+        throw err; 
+    } ;
+
+    files.forEach(file => {
+        fs.copyFile( folderName + `/${file.name}`, folderNameNew + `/${file.name}` , (err) => {
+            if (err) {
+              console.log("Error Found:", err);
+            }
+
+    });
+
+}) ;
+
+}) ;
