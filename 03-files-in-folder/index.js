b31aee463d8bd7026fe8589496c942e6cff23dc0
SergOswald <skobrusev446@gmail.com>
2022-05-24 21:39:06 +0300

    fix: extension 24:05:22 21:39

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -9,7 +9,7 @@ const path1 = path.format({
     //base: 'text.txt',
 });
 
-
+/*
 fs.readdir( "./03-files-in-folder/secret-folder" , (err, data) => {
     if (err) console.log(err) ;
   	let len = data.length ;
@@ -20,8 +20,43 @@ fs.readdir( "./03-files-in-folder/secret-folder" , (err, data) => {
             var siz = stats.size;
             
         }) ;
-*/
+
         console.log( data[ i ].slice( 0 , data[ i ].length - 3 ) + " - " + data[ i ].slice( -3 ) + " - " ) ;
 
       }
 })
+
+*/
+
+
+
+let folderName = "./03-files-in-folder/secret-folder" ;
+
+fs.readdir(folderName, {withFileTypes: true}, (err, files) => {
+    if (err) {
+        throw err; 
+    } ;
+
+    files.forEach(file => {
+        //console.log(file.isDirectory());
+        if( file.isFile() ) {
+            var ext = path.extname( file.name );
+            
+            /*
+            fs.stat( file , (err, stats ) => {
+                if (err) {
+                  console.error(err);
+                } ;
+                
+            });
+
+            */
+
+            console.log( file.name.slice( 0 , file.name.length - ext.length ) + " - " +  ext + " - "  );
+        }
+        //console.log(file.isFile());
+
+        
+
+    });
+}) ;
