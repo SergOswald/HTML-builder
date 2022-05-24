72e79034c09a2668a40188cc54ea83d92ef4ec91
SergOswald <skobrusev446@gmail.com>
2022-05-24 21:56:22 +0300

    feat: 03-files last 24-05-22 21:56

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,33 +1,11 @@
 var fs = require('fs');
 
-
-
 const path = require('path');
 
 const path1 = path.format({
     dir: './03-files-in-folder/secret-folder',
     //base: 'text.txt',
 });
-
-/*
-fs.readdir( "./03-files-in-folder/secret-folder" , (err, data) => {
-    if (err) console.log(err) ;
-  	let len = data.length ;
-
-      for( let i = 0 ; i < len ; i++ ) {
-          /*
-        fs.stat(path1, function(err, data[ i ] ) {
-            var siz = stats.size;
-            
-        }) ;
-
-        console.log( data[ i ].slice( 0 , data[ i ].length - 3 ) + " - " + data[ i ].slice( -3 ) + " - " ) ;
-
-      }
-})
-
-*/
-
 
 
 let folderName = "./03-files-in-folder/secret-folder" ;
@@ -42,21 +20,17 @@ fs.readdir(folderName, {withFileTypes: true}, (err, files) => {
         if( file.isFile() ) {
             var ext = path.extname( file.name );
             
-            /*
-            fs.stat( file , (err, stats ) => {
+            
+            fs.stat( "./03-files-in-folder/secret-folder" + `/${file.name}` , (err, stats ) => {
                 if (err) {
                   console.error(err);
                 } ;
-                
+                console.log( file.name.slice( 0 , file.name.length - ext.length ) + " - " +  ext + " - "  + stats.size + "kb" );
             });
-
-            */
-
-            console.log( file.name.slice( 0 , file.name.length - ext.length ) + " - " +  ext + " - "  );
+       
+            
         }
-        //console.log(file.isFile());
-
-        
+               
 
     });
 }) ;
