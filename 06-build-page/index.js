8a2597110b31751e619afd7d94ecfcc833869d2e
SergOswald <skobrusev446@gmail.com>
2023-05-08 23:33:01 +0300

    feat: add task 6 (8-05-2023 11:31 pm)

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -3,14 +3,21 @@ const path = require('path');
 
 let folderName = "./06-build-page/project-dist" ;
 
+let assetsfolderName = "./06-build-page/assets" ;
+
+let newassetsfolderName = "./06-build-page/project-dist/assets" ;
+
 let oldfileName = "./06-build-page/template.html";
 
 let newfileName = "./06-build-page/project-dist/index.html";
 
-const path1 = path.format({
-    dir: folderName,
-  //  base: 'text.txt',
-});
+let blockName = "./06-build-page/components/";
+
+
+// const path1 = path.format({
+//     dir: folderName,
+//   //  base: 'text.txt',
+// });
 
 fs.mkdir( folderName, err => {
    if( err ) throw err; 
@@ -20,4 +27,119 @@ fs.mkdir( folderName, err => {
 fs.copyFile( oldfileName, newfileName, err => {
     if( err ) throw err; 
     console.log('Файл "template.html" успешно скопирован в папку "project-dist" под именем "index.html" ');
- });
+});
+
+//var writeableStream = fs.createWriteStream( newfileName );
+
+/*
+
+fs.readFile( newfileName, 'utf8', (error, fileContent) => {
+
+  fs.readFile( "./06-build-page/components/header.html", 'utf8', (errorHeader, fileContentHeader) => {
+  if(errorHeader) throw errorHeader;
+  fileContent = fileContent.replace(/\{\{header\}\}/, fileContentHeader);
+  writeableStream.write(fileContent);
+  writeableStream.end();
+  });
+
+});
+
+*/
+
+/*
+
+fs.mkdir( path.join( folderName, 'assets'), ( err ) => {
+   if ( err ) { return console.error( err ); };
+   console.log( 'Directory "assets" created successfully!' );
+});
+
+fs.readdir( assetsfolderName, { withFileTypes: true }, ( err, files) => {
+  if ( err ) { throw err; };
+
+files.forEach( file => {
+fs.copyFile( assetsfolderName + `/${file.name}`, newassetsfolderName + `/${file.name}` , 
+(err) => { if ( err ) { console.log( "Error Found:" , err ); } });
+console.log( 'Файлы скопірованы успешно в новую папку "assets" !' );
+}) ;
+
+}) ;
+
+*/
+
+//====
+// перемещение всех стилей в заданную папку в один файл
+
+let stylesfolderName = "./06-build-page/styles" ;
+
+let stylesfolderNameNew = "./06-build-page/project-dist/styles" ;
+
+fs.mkdir( stylesfolderNameNew, err => {
+   if( err ) throw err; 
+   console.log( 'Папка "project-dist/styles" успешно создана' );
+});
+
+const path1 = path.format({
+    dir: stylesfolderName ,
+    base: "footer.css",
+});
+
+const path2 = path.format({
+    dir: stylesfolderName ,
+    base: "header.css",
+});
+
+const path3 = path.format({
+    dir: stylesfolderName ,
+    base: "main.css",
+});
+
+const total = path.format({
+    dir: stylesfolderNameNew,
+    base: "style.css",
+});
+
+var writeableStream = fs.createWriteStream( total );
+
+
+function fileHandler(){
+
+fs.readFile( path1, 'utf8', ( err, fileContent ) => {
+  if( err ) throw err;
+  let toWrite1 = fileContent;
+
+fs.writeFile( total , toWrite1 , function( error ){
+  if( err ) throw err;
+  writeableStream.write( toWrite1 );
+});
+ 
+});
+
+fs.readFile( path2, 'utf8', ( err, fileContent ) => {
+  if( err ) throw err;
+  let toWrite2 = fileContent;
+  
+  fs.writeFile( total , toWrite2, function( error ){
+    if( err ) throw err;
+    writeableStream.write( toWrite2 );
+  });
+
+});
+
+fs.readFile( path3, 'utf8', ( err, fileContent ) => {
+  if( err ) throw err;
+  let toWrite3 = fileContent;
+
+  fs.writeFile( total , toWrite3, function( error ){
+    if( err ) throw err;
+    writeableStream.write( toWrite3 );
+  });
+
+  console.log( 'Стили успешно скопированы в один файл "project-dist/styles/styles.css" ' );  
+
+});
+
+};
+
+fileHandler();
+
+//====
