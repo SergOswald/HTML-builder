214caed717ee8bd0ec1730db5afdf8650e55ff22
SergOswald <skobrusev446@gmail.com>
2023-05-09 23:32:30 +0300

    feat: add html ub in task6 (9-05-2023 11:32 pm)

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,12 +1,6 @@
 var fs = require('fs');
 const path = require('path');
 
-let folderName = "./06-build-page/project-dist" ;
-
-let oldfileName = "./06-build-page/template.html";
-
-let newfileName = "./06-build-page/project-dist/index.html";
-
 let blockName = "./06-build-page/components/";
 
 
@@ -15,59 +9,20 @@ let blockName = "./06-build-page/components/";
 //   //  base: 'text.txt',
 // });
 
-fs.mkdir( folderName, err => {
-   if( err ) throw err; 
-   console.log( 'Папка "project-dist" успешно создана' );
-});
-
-fs.copyFile( oldfileName, newfileName, err => {
-    if( err ) throw err; 
-    console.log('Файл "template.html" успешно скопирован в папку "project-dist" под именем "index.html" ');
-});
-
-//var writeableStream = fs.createWriteStream( newfileName );
-
-/*
-
-fs.readFile( newfileName, 'utf8', (error, fileContent) => {
-
-  fs.readFile( "./06-build-page/components/header.html", 'utf8', (errorHeader, fileContentHeader) => {
-  if(errorHeader) throw errorHeader;
-  fileContent = fileContent.replace(/\{\{header\}\}/, fileContentHeader);
-  writeableStream.write(fileContent);
-  writeableStream.end();
-  });
-
-});
-
-*/
-
-/*
-
-fs.mkdir( path.join( folderName, 'assets'), ( err ) => {
-   if ( err ) { return console.error( err ); };
-   console.log( 'Directory "assets" created successfully!' );
-});
-
-fs.readdir( assetsfolderName, { withFileTypes: true }, ( err, files) => {
-  if ( err ) { throw err; };
-
-files.forEach( file => {
-fs.copyFile( assetsfolderName + `/${file.name}`, newassetsfolderName + `/${file.name}` , 
-(err) => { if ( err ) { console.log( "Error Found:" , err ); } });
-console.log( 'Файлы скопірованы успешно в новую папку "assets" !' );
-}) ;
-
-}) ;
-
-*/
-
 //====
 // перемещение всех стилей в заданную папку в один файл
 
 let stylesfolderName = "./06-build-page/styles" ;
 
 let stylesfolderNameNew = "./06-build-page/project-dist/styles" ;
+
+let folderName = "./06-build-page/project-dist" ;
+
+
+fs.mkdir( folderName, err => {
+  if( err ) throw err; 
+  console.log( 'Папка "project-dist" успешно создана' );
+});
 
 fs.mkdir( stylesfolderNameNew, err => {
   if( err ) throw err; 
@@ -93,7 +48,7 @@ const total = path.format({
     base: "style.css",
 });
 
-var writeableStream = fs.createWriteStream( total );
+var writeableStream2 = fs.createWriteStream( total );
 
 
 function fileHandler(){
@@ -104,7 +59,7 @@ fs.readFile( path1, 'utf8', ( err, fileContent ) => {
 
 fs.writeFile( total , toWrite1 , function( error ){
   if( err ) throw err;
-  writeableStream.write( toWrite1 );
+  writeableStream2.write( toWrite1 );
 });
  
 });
@@ -115,7 +70,7 @@ fs.readFile( path2, 'utf8', ( err, fileContent ) => {
   
   fs.writeFile( total , toWrite2, function( error ){
     if( err ) throw err;
-    writeableStream.write( toWrite2 );
+    writeableStream2.write( toWrite2 );
   });
 
 });
@@ -126,7 +81,7 @@ fs.readFile( path3, 'utf8', ( err, fileContent ) => {
 
   fs.writeFile( total , toWrite3, function( error ){
     if( err ) throw err;
-    writeableStream.write( toWrite3 );
+    writeableStream2.write( toWrite3 );
   });
 
   console.log( 'Стили успешно скопированы в один файл "project-dist/styles/styles.css" ' );  
@@ -135,7 +90,7 @@ fs.readFile( path3, 'utf8', ( err, fileContent ) => {
 
 };
 
-fileHandler();
+//fileHandler();
 
 //====
 
@@ -196,3 +151,60 @@ fs.readdir( "./06-build-page/assets/svg", { withFileTypes: true }, ( err, files)
 }) ;
 
 //====
+
+//====
+// Добавление блоков в project-dist/index.html
+
+let oldfileName = "./06-build-page/template.html";
+
+let newfileName = "./06-build-page/project-dist/index.html";
+
+fs.copyFile( oldfileName, newfileName, err => {
+  if( err ) throw err; 
+  console.log('Файл "template.html" успешно скопирован в папку "project-dist" под именем "index.html" ');
+});
+
+
+var writeableStream = fs.createWriteStream( newfileName );
+
+fs.readFile( newfileName, 'utf8', (error, fileContent) => {
+
+  fs.readFile( "./06-build-page/components/header.html", 'utf8', (errorHeader, fileContentHeader) => {
+  if( errorHeader ) throw errorHeader;
+  fileContent = fileContent.replace( /\{\{header\}\}/, fileContentHeader );
+  writeableStream.write(fileContent);
+  //writeableStream.end();
+
+  });
+
+});
+
+/*
+fs.readFile( newfileName, 'utf8', (error, fileContent) => {
+
+  fs.readFile( "./06-build-page/components/articles.html", 'utf8', (errorHeader, fileContentHeader) => {
+    if( errorHeader ) throw errorHeader;
+    fileContent = fileContent.replace( /\{\{articles\}\}/, fileContentHeader );
+    writeableStream.write(fileContent);
+    //writeableStream.end();
+  });
+
+});
+
+/*
+
+var writeableStream = fs.createWriteStream( newfileName );
+
+fs.readFile( newfileName, 'utf8', (error, fileContent) => {
+
+  fs.readFile( "./06-build-page/components/footer.html", 'utf8', (errorHeader, fileContentHeader) => {
+    if( errorHeader ) throw errorHeader;
+    fileContent = fileContent.replace( /\{\{footer\}\}/, fileContentHeader );
+    writeableStream.write(fileContent);
+    writeableStream.end();
+  });
+
+});
+
+*/
+fileHandler();
